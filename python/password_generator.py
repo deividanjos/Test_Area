aca46aac7168693d54567867308cb09e4fc73b25
deividanjos <deividanjos057@gmail.com>
2022-02-12 19:06:56 -0300

    Modified file

--- python/password_generator.py
+++ python/password_generator.py
@@ -2,21 +2,42 @@ import random
 from string import digits
 from string import ascii_letters
 from string import punctuation
+import PySimpleGUI as sg
 
-# inputs 
-escolha = int(input("Qual tipo de senha vc deseja: "))
-q_c = int(input("Qual a quantidade de caracteres vc deseja: "))
 
-# tipos de senhas
-if escolha == 1:
-  senha = digits
-elif escolha == 2:
-  senha = digits + ascii_letters
-elif escolha == 3:
-  senha = digits + ascii_letters + punctuation
+# Tema 
+sg.theme("DarkAmber")
 
-# gerador de senha 
-secure_random = random.SystemRandom()
-password = "".join(secure_random.choice(senha)
-                  for i in range(q_c))
-print(password)
+# Todas as coisas que vão aparecer na janela!
+layout = [ [sg.Text("1. Senha só com números.")],
+           [sg.Text("2. Senha com números e letras.")],
+           [sg.Text("3. Senha com números, letras e caracteres especiais.")],
+           [sg.Text("4. Senha só com letras.")],
+           [sg.Text("5. Senha com letras é caracteres especiais.\n")],
+           [sg.Text("Escolha o que você deseja: "), sg.Input()],
+           [sg.Text("Qual a Quantidade de caracteres: "), sg.Input()],
+           [sg.Button("Ok"), sg.Button("Cancel")] ]
+
+# Criando a Janela
+window = sg.Window("Gerador de Sanhas", layout)
+
+# Event Loop para processar "eventos" e obter os "valores" das entradas.
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Cancel":
+        break
+
+    if int(values[0]) == 1:
+        senha = digits
+    elif int(values[0]) == 2:
+        senha = digits + ascii_letters
+    elif int(values[0]) == 3:
+        senha = digits + ascii_letters + punctuation
+    # gerador de senha 
+    secure_random = random.SystemRandom()
+    password = "".join(secure_random.choice(senha)
+                      for i in range(int(values[1])))
+
+    print("Sua Senha é: ", password)
+
+window.close()
